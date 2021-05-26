308d22132b5c54bfa2759bf182d983c21096644f
chaytee <84829558+chaytee@users.noreply.github.com>
2021-05-26 15:54:11 +0900

    修正しました。

--- src/index.js
+++ src/index.js
@@ -1,10 +1,41 @@
-import "./styles.css";
+const onClickAdd = () => {
+  const inputText = document.getElementById("add-txt").value;
+  //add-btnを押すと初期化
+  document.getElementById("add-txt").value = "";
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+  //liタグ生成
+  const li = document.createElement("li");
+
+  //div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+  div.innerHTML = inputText;
+
+  //liタグの子要素に各要素(list-row)を設定
+  li.appendChild(div);
+
+  //buttnタグ(完了)生成
+  const completeBtn = document.createElement("button");
+  completeBtn.innerText = "完了";
+  completeBtn.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //buttnタグ（削除）生成
+  const deleteBtn = document.createElement("button");
+  deleteBtn.innerText = "削除";
+  deleteBtn.addEventListener("click", () => {
+    alert("完了");
+  });
+
+  //div(list-row)の子要素に下記を追加
+  div.appendChild(completeBtn);
+  div.appendChild(deleteBtn);
+
+  //未完了のリストに追加
+  document.getElementById("incompleate-list").appendChild(li);
+};
+
+document
+  .getElementById("add-btn")
+  .addEventListener("click", () => onClickAdd());
