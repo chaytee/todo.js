e5e9baf436890802b1e6cb34c5952598bccd1bbf
chaytee <84829558+chaytee@users.noreply.github.com>
2021-05-27 15:38:49 +0900

    補足つけました。

--- src/index.js
+++ src/index.js
@@ -5,22 +5,25 @@ const onClickAdd = () => {
   //未完了リストに追加するを実行
   createIncompleateList(inputText);
 };
-//未完了リストから指定の要素を削除(共通関数)
+
+//------未完了リストから指定の要素を削除(共通関数)
 const deleatFromIncompleateList = (target) => {
   document.getElementById("incompleate-list").removeChild(target);
 };
-//未完了リストに追加する（共通関数）
+
+//------未完了リストに追加する（共通関数）
+//li>.list-low>p.todo-name+button*2
 const createIncompleateList = (text) => {
   //liタグ生成
   const li = document.createElement("li");
 
-  //ｐタグ生成
-  const todoName = document.createElement("p");
-  todoName.className = "todo-name";
-
   //div生成list-row
   const div = document.createElement("div");
   div.className = "list-row";
+
+  //ｐタグ生成
+  const todoName = document.createElement("p");
+  todoName.className = "todo-name";
 
   //liタグの子要素に各要素(div.list-row)を設定
   //その中にpタグを作成しinputTextを入れる
@@ -32,19 +35,19 @@ const createIncompleateList = (text) => {
   const completeBtn = document.createElement("button");
   completeBtn.innerText = "完了";
 
-  //完了ボタン押したら
+  //完了ボタン押したら--未完了から削除して完了リストへ
   completeBtn.addEventListener("click", () => {
     //押された完了ボタンの親の親タグ(li) を未完了リストから削除
     deleatFromIncompleateList(deleteBtn.parentNode.parentNode);
 
-    //完了リストに追加する要素 div.list-low）
+    //完了リストに追加する要素 div.list-low）を使いまわし
     const addTarget = completeBtn.parentNode;
 
     //liタグ生成
     const li = document.createElement("li");
     li.appendChild(addTarget);
 
-    //todo内容テキストを取得
+    //todo内容テキストを取得(.todo-name)
     const text = addTarget.firstElementChild.innerHTML;
 
     //addTarget初期化
@@ -52,13 +55,17 @@ const createIncompleateList = (text) => {
 
     //pタグの生成
     const todoTitle = document.createElement("p");
+    todoName.className = "todo-name";
     addTarget.appendChild(todoTitle);
     todoTitle.innerHTML = text;
 
-    //戻すボタン
+    //戻すボタンの生成--完了リストから削除し未完了リストへ
     const backBtn = document.createElement("button");
     backBtn.innerHTML = "戻す";
     addTarget.appendChild(backBtn);
+
+    //完了リストに追加--場所を明記
+    document.getElementById("compleate-list").appendChild(li);
 
     //戻すボタンを押したら
     backBtn.addEventListener("click", () => {
@@ -67,10 +74,9 @@ const createIncompleateList = (text) => {
       document.getElementById("compleate-list").removeChild(deleteTarget);
       //テキスト取得
       const text = backBtn.parentNode.firstElementChild.innerHTML;
+      //未完了リストに追加の関数を実行
       createIncompleateList(text);
     });
-    //完了リストに追加
-    document.getElementById("compleate-list").appendChild(li);
   });
 
   //buttnタグ（削除）生成
